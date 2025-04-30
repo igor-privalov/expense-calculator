9e3e3a35d35eca1d4e35796daf2e3106ef36449f
Ihar Pryvalau <ihar.pryvalau@innowise.com>
2025-04-30 09:15:12 +0200

    Init commit with backend project structure

---
+++ expensio-backend/src/main/java/com/innowise/expensio/ExpensioApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.expensio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensioApplication.class, args);
+	}
+
+}
---
+++ expensio-backend/src/test/java/com/innowise/expensio/ExpensioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.expensio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
